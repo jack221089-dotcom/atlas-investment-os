c45b4652f22fff5b784cb39ec51299b7cf96fc71
jack221089-dotcom <jack221089@gmail.com>
2026-08-04 21:03:50 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "atlas-3.0.0-alpha20-1";
+const CACHE_NAME = "atlas-3.0.0-alpha20-2";
 
 const scopeUrl = new URL(self.registration.scope);
 const assetUrl = path => new URL(path, scopeUrl).toString();
